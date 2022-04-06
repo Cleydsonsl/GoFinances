661845d3dd8c7257002a92ebbdf39aaba799593c
CleydsonSl <cley.souza007@gmail.com>
2022-04-06 11:46:01 -0300

    add fonts

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Title } from "./styles";
 
 export function Dashboard() {
   return (
-    <Container></Container>
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
   )
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,3 +1,13 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.blue_primary};
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.Text`
+  font-family: ${({ theme })=> theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.shape};
+`
